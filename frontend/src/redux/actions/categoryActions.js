bdbcb669c292fc3b48b9e3a42b3b71ff7cf1f8f9
unknown <agatalipiak25@gmail.com>
2021-04-18 14:55:13 +0200

    return to old env

--- frontend/src/redux/actions/categoryActions.js
+++ frontend/src/redux/actions/categoryActions.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { CATEGORY_LOADING, GET_ALL_CATEGORIES, GET_CATEGORY_FOR_SECTION } from "./types";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
 // Set loading state
 export const setCategoryLoading = () => ({
     type: CATEGORY_LOADING,
@@ -14,7 +12,7 @@ export const setCategoryLoadingAction = () => (dispatch) => {
 // Get Category
 export const getAllCategories = () => (dispatch) => {
     axios
-        .get(`${apiUrl}/api/category/categories`)
+        .get("/api/category/categories")
         .then(res => dispatch({
             type: GET_ALL_CATEGORIES,
             payload: {
@@ -32,7 +30,7 @@ export const getAllCategories = () => (dispatch) => {
 // Get Category for section
 export const getCategoryForSection = section => (dispatch) => {
     axios
-        .get(`${apiUrl}/api/category/${section}`)
+        .get(`/api/category/${section}`)
         .then(res => dispatch({
             type: GET_CATEGORY_FOR_SECTION,
             payload: res.data,
